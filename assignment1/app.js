cbae40d1ca3b0f75dbeb1011f2e30ef61b6debcc
rostasan <ross.dickman@gmail.com>
2016-09-18 21:39:58 -0500

    added comment.

--- assignment1/app.js
+++ assignment1/app.js
@@ -8,10 +8,10 @@
 
   function LunchCheckController($scope) {
 
+// I'm having trouble figuring out how to make this function with the split() javascript function.
 
-  console.log("Hello");
-        $scope.tooMuch = function(str){
-          $scope.lunchItem = str.split(",");
+        $scope.tooMuch = function(lunchItem){
+          var str = lunchItem.split(",", 10);
           if($scope.lunchItem  > 4){
             $scope.message = "Enjoy";
           }else {
